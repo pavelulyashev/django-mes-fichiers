433ce511a7c9faab3baf4fb81a7a9bb78515e19d
Pavel Ulyashev <p.ulyashev@gmail.com>
2013-05-17 11:12:40 +0700

    Add angular resources for album and files

--- src/apps/file_uploader/urls.py
+++ src/apps/file_uploader/urls.py
@@ -1,25 +1,15 @@
 from django.conf.urls import patterns, url, include
 from rest_framework.routers import DefaultRouter
 from src.apps.file_uploader import views
-from src.apps.file_uploader.views import album_detail, album_create, album_update, album_delete, file_create, file_detail, file_update, file_delete
 
 
-urlpatterns1 = patterns('',
-    url(r'^album/create/$', album_create, name='album_create'),
-    url(r'^album/(?P<pk>[0-9]+)/$', album_detail, name='album_detail'),
-    url(r'^album/(?P<pk>[0-9]+)/update/$', album_update, name='album_update'),
-    url(r'^album/(?P<pk>[0-9]+)/delete/$', album_delete, name='album_delete'),
-
-    url(r'^file/create/$', file_create, name='file_create'),
-    url(r'^file/(?P<pk>[0-9]+)/$', file_detail, name='file_detail'),
-    url(r'^file/(?P<pk>[0-9]+)/update/$', file_update, name='file_update'),
-    url(r'^file/(?P<pk>[0-9]+)/delete/$', file_delete, name='file_delete'),
-
-    url(r'^(?P<path>.*)$', views.RootView.as_view(), name='file_uploader_root'),
-)
+class CustomRouter(DefaultRouter):
+    def __init__(self):
+        super(CustomRouter, self).__init__()
+        self.routes[0] = self.routes[0]._replace(url=r'^{prefix}/?$')
 
 
-router = DefaultRouter()
+router = CustomRouter()
 router.register(r'albums', views.AlbumViewSet)
 router.register(r'files', views.FileViewSet)
 
--- src/apps/file_uploader/views.py
+++ src/apps/file_uploader/views.py
@@ -1,5 +1,3 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView
 
 from rest_framework import viewsets
@@ -14,17 +12,7 @@ class AlbumViewSet(viewsets.ModelViewSet):
     queryset = MonAlbum.objects.all()
     model = MonAlbum
 
-album_detail = AlbumViewSet.as_view({'get': 'retrieve'})
-album_create = AlbumViewSet.as_view({'post': 'create'})
-album_update = AlbumViewSet.as_view({'post': 'update'})
-album_delete = AlbumViewSet.as_view({'post': 'destroy'})
-
 
 class FileViewSet(viewsets.ModelViewSet):
     queryset = MonFile.objects.all()
     model = MonFile
-
-file_create = FileViewSet.as_view({'post': 'create'})
-file_detail = FileViewSet.as_view({'get': 'retrieve'})
-file_update = FileViewSet.as_view({'post': 'update'})
-file_delete = FileViewSet.as_view({'post': 'destroy'})
