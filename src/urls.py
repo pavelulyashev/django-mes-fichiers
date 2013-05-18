1b15cd522ff2ad28d18fe853576c7eb7e78c3ed9
Pavel Ulyashev <p.ulyashev@gmail.com>
2013-05-19 03:36:44 +0700

    Serve media files when under runserver

--- src/urls.py
+++ src/urls.py
@@ -1,13 +1,18 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
+from django.conf.urls.static import static
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^file_uploader/', include('src.apps.file_uploader.urls')),
 
-
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
